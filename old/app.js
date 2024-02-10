7b90c36dec03db3d7b4c91daeca30e0fc154225a
Daniel Fahrni <dafahrni@hotmail.com>
2024-02-10 21:53:55 +0100

    obsolete code removed

--- old/app.js
+++
@@ -1,8 +0,0 @@
-import { GameView } from "./views/game-view.js";
-import { GameModel } from "./models/game-model.js";
-import  { GameController} from "./controllers/game-controller.js";
-
-const model = new GameModel(4);
-const view = new GameView(model);
-const controller = new GameController(model, view);
-controller.run();
