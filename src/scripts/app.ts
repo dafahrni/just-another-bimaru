3935162e4a6bd4f2f104fb85978958ba64b9ce1d
Daniel Fahrni <dafahrni@hotmail.com>
2024-02-16 22:22:41 +0100

    after finished game, predefined cells are displayed

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -25,26 +25,23 @@ new App();
 // ---------------
 // TODO: allocate player name and date with the game
 // TODO: keep history of game (e.g. to navigate between the moves or for speed analysis)
-// TODO: create API with source for game definitions
 // TODO: persist game definitions on local store
 // TODO: check edited game with solver if valid
 // TODO: create game definition editor
 // TODO: play sound during the game (can be turned on/off)
-// TODO: consider to correct ships when water is placed
+// TODO: create API with source for game definitions
 // TODO: create API with node.js and use Unit Tests in Postman
 // TODO: implement scaling according to screen size
 
 // I S S U E S
 // -----------
-// TODO: usage of ship rotation also rotates water tiles accidentally
+// TODO: usage of ship rotation sometimes also rotates water tiles accidentally
+// TODO: consider to correct ships when water is placed
 // TODO: placement of water tiles does not correct ships with direction
 // TODO: placement of tiles should be as free as possible (keep the flow in the game)
-// TODO: placement of lines with water only when line is fullfilled (has √ mark)
 
 // I M P R O V E M E N T S
 // -----------------------
-// TODO: setup build pipeline with unit tests
-// TODO: enable debugging in TS files with source mapping
 // TODO: document message flow of cell selection between view and model
 // TODO: merge Game and GameModel
 // TODO: simplify naming (make it consistent, use same name for same things!)
--- src/scripts/models/board/parts/cell.ts
+++ src/scripts/models/board/parts/cell.ts
@@ -148,6 +148,7 @@ export class Cell {
   }
 
   reset() {
+    if (this.isFix) return;
     this.value = CellValue.empty;
   }
 
