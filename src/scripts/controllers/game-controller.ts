a2b2157c8fb070f5e4e24c6590f36f0b734e03b5
Daniel Fahrni <dafahrni@hotmail.com>
2024-02-21 18:26:49 +0100

    messaging applied in controller

--- src/scripts/controllers/game-controller.ts
+++ src/scripts/controllers/game-controller.ts
@@ -1,16 +1,23 @@
-import { GameModel } from "../models/game-model";
-import { GameView } from "../views/game-view";
+import { DtoFactory } from "../models/dtos/dto-factory.js";
+import { GameModel } from "../models/game-model.js";
+import { Broker } from "../models/messaging/broker.js";
+import { Message } from "../models/messaging/message.js";
+import { GameView } from "../views/game-view.js";
 
 export class GameController {
 
   private model: GameModel;
   private view: GameView;
+  private broker: Broker = Broker.get();
   
   constructor(model: GameModel, view: GameView) {
     this.model = model;
     this.view = view;
     this.view.bindSelectionChanged((i: number) => this.nextMove(i));
     this.view.bindLabelClick((i: number) => this.fillLineWithWater(i));
+
+    const dto = DtoFactory.mapGame(this.model);
+    this.broker.publish(Message.newGame(dto));
   }
 
   fillLineWithWater(index: number) {
@@ -28,7 +35,10 @@ export class GameController {
 
     if (this.model.checkForWinner()) {
       this.view.gameIsWon();
-      this.model.resetCells();
+      this.model.resetCells(); // TODO: remove this line
+
+      const dto = DtoFactory.mapGame(this.model);
+      this.broker.publish(Message.newGame(dto));
     }
   }
 
