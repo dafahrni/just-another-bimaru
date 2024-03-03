0cbc1c89448e38643a3d7ca059be52ace4b0764e
Daniel Fahrni <dafahrni@hotmail.com>
2024-03-03 11:38:11 +0100

    implementation of edit view started...

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -27,6 +27,8 @@ new App().init();
 
 // F E A T U R E S
 // ---------------
+// TODO: draw ships to find and keep statistics (4-1, 3-2, etc.)
+// TODO: add splash screen with sound
 // TODO: allocate player name and date with the game
 // TODO: keep history of game (e.g. to navigate between the moves or for speed analysis)
 // TODO: persist game definitions on local store
@@ -53,3 +55,29 @@ new App().init();
 //       - e.g. Grid, Field, Board, Bimaru
 //       - e.g. Tile, Parent, Cell, CellElement, ShipCell
 //       - e.g. CellType, Symbol, ch, Value
+
+// V I E W - C O N C E P T
+// -----------------------
+// The game-view containes all view elements, such as menu, options, board, edit-view etc.
+// To enter e new GameDefinition, the edit-view requires some input forms and the board to 
+// display the user's inputs in order to provide some sort of feedback to the user. 
+// Those forms could be placed in a configuration-view on the right side of the screen 
+// (similar to the menu-view on the left side).
+// The board, therefore, must distinguish between game-mode and edit-mode.
+
+// FUI (=flow of user interactions, wizard steps)
+// 1. definition of grid size (offer: 8..12, default: 10)
+// 2. entering of ship amounts (offer only default, readonly)
+// 3. entering of row targets (offer: 0..8, default: 0)
+// 4. entering of col targets (offer: 0..8, default: 0)
+// 5. entering of predefined cells (select in grid)
+
+// TODO: take-over swipe stuff from menu-view for config-view
+// TODO: implement FUI (should be mix between modal dialog and selection in grid)
+// TODO: use background color to avoid confusion between game and edit view
+// TODO: use bread crumbs to show the steps of the during FUI in the wizard
+// TODO: navigation between pages/views via button menu (access via slide)
+// TODO: there should be only 1 MVC per page, every page consists of
+//       - model, i.e. GameModel, EditModel
+//       - view, i.e. GameView, EditView
+//       - controller, i.e. GameCntr, EditCntr
---
+++ src/scripts/views/edit/value-control.ts
@@ -0,0 +1,50 @@
+class ValueControl {
+
+    private inputElement: HTMLInputElement;
+    private decreaseButton: HTMLButtonElement;
+    private increaseButton: HTMLButtonElement;
+
+    constructor(inputId: string, decreaseButtonId: string, increaseButtonId: string) {
+        this.inputElement = document.getElementById(inputId) as HTMLInputElement;
+        this.decreaseButton = document.getElementById(decreaseButtonId) as HTMLButtonElement;
+        this.increaseButton = document.getElementById(increaseButtonId) as HTMLButtonElement;
+
+        this.decreaseButton.addEventListener("click", this.decreaseValue.bind(this));
+        this.increaseButton.addEventListener("click", this.increaseValue.bind(this));
+    }
+
+    get value(): number {
+        return parseInt(this.inputElement.value);
+    }
+
+    set value(value: string) {
+        this.inputElement.value = `${value}`;
+    }
+
+    registerForInputChanges(type: string, listener: EventListenerOrEventListenerObject) {
+        this.inputElement.addEventListener(type, listener);
+    }
+
+    private decreaseValue() {
+        let currentValue = parseInt(this.inputElement.value);
+        if (currentValue > parseInt(this.inputElement.min)) {
+            currentValue--;
+            this.inputElement.value = String(currentValue);
+            this.dispatchEvent();
+        }
+    }
+
+    private increaseValue() {
+        let currentValue = parseInt(this.inputElement.value);
+        if (currentValue < parseInt(this.inputElement.max)) {
+            currentValue++;
+            this.inputElement.value = String(currentValue);
+            this.dispatchEvent();
+        }
+    }
+
+    private dispatchEvent() {
+        const event = new Event("input", { bubbles: true });
+        this.inputElement.dispatchEvent(event);
+    }
+}
