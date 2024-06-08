93bb59a5bea6492a7540c89fc20832b137f7d2a6
Daniel Fahrni <dafahrni@hotmail.com>
2024-06-08 11:45:35 +0200

    config view enabled

--- src/scripts/views/forms/side-view.ts
+++ src/scripts/views/forms/side-view.ts
@@ -108,9 +108,8 @@ export class SideView {
         this.viewMode = ViewMode.ContentOnly;
         break;
       case ViewMode.ContentOnly:
-        // TODO: un-comment if conig view is should be accessible
-        //this.openConfig();
-        //this.viewMode = ViewMode.ContentAndRightSideView;
+        this.openConfig();
+        this.viewMode = ViewMode.ContentAndRightSideView;
         break;
       case ViewMode.ContentAndRightSideView:
         // do nothing
